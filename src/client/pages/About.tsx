7f5b329cf73d471b62245b75c91d3f6b05d4dc9c
Анастасия <71327963+anasteisha-4@users.noreply.github.com>
2025-07-20 14:46:35 +0300

    Update About.tsx (wait test failed)

--- src/client/pages/About.tsx
+++ src/client/pages/About.tsx
@@ -12,7 +12,7 @@ export const About: React.FC = () => {
         <div className="col">
           <h1>About</h1>
           <p>
-            Author: Никита Жуйков
+            Author: test
           </p>
         </div>
       </div>
