72e1d605a2660a745041447e568fdf8bc4b62231
Анастасия <71327963+anasteisha-4@users.noreply.github.com>
2025-07-20 14:54:19 +0300

    Update about.test.tsx

--- test/about.test.tsx
+++ test/about.test.tsx
@@ -6,6 +6,6 @@ describe('About', () => {
     it('Should render author name', () => {
         const { container } = render(<About />);
 
-        expect(container.textContent).toBe('AboutAuthor: Никита Жуйков');
+        expect(container.textContent).toBe('AboutAuthor: test');
     });
 });
